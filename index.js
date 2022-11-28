657bc5e70465fe5c1db624cc2a90ba32c62c3ba4
Alejandro <Alejandrodfco@gmail.com>
2022-11-28 23:01:46 +0100

    Cuarta version del proyecto

---
+++ index.js
@@ -0,0 +1,38 @@
+let parrafos = document.getElementsByTagName("p");
+let enlaces = document.getElementsByTagName("a");
+let parrafoCero = document.getElementsByTagName("p")[0].innerHTML;
+
+parrafos[1].style.visibility = "hidden";
+parrafos[2].style.visibility = "hidden";
+parrafos[3].style.visibility = "hidden";
+parrafos[4].style.visibility = "hidden";
+
+
+enlaces[2].addEventListener("click", function(){
+    getParrafo(0);
+    
+});
+enlaces[3].addEventListener("click", function(){
+    getParrafo(1);
+    
+});
+enlaces[4].addEventListener("click", function(){
+    getParrafo(2);
+    
+});
+enlaces[5].addEventListener("click", function(){
+    getParrafo(3);
+    
+});
+enlaces[6].addEventListener("click", function(){
+    getParrafo(4);
+    
+})
+
+function getParrafo(indice) {
+    if( indice == 0 ){
+        parrafos[0].innerHTML = parrafoCero;
+    }
+    parrafos[0].innerHTML = parrafos[indice].innerHTML
+}
+
